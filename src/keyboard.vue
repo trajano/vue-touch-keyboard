9cd4bfea744e479ac87d2dc4a8e46456ba82e128
Archimedes Trajano <developer@trajano.net>
2017-12-30 11:17:53 -0500

    Changed line index key
    This is to prevent the duplicate key warnings

--- src/keyboard.vue
+++ src/keyboard.vue
@@ -2,7 +2,7 @@
 	.vue-touch-keyboard
 		// input(type="text", v-model="keyboardText", v-if="!input")
 		.keyboard
-			.line(v-for="(line, index) in keySet", key="index")
+			.line(v-for="(line, lindex) in keySet", key="lindex")
 				span(v-for="(key, index) in line", key="index", :class="getClassesOfKey(key)", v-text="getCaptionOfKey(key)", @click="clickKey(key)", @mousedown="mousedown", :style="getKeyStyle(key)")
 
 </template>
